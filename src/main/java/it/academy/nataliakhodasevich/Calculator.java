bdb25ef648d09b0e90ead1446658931cbf56df44
n.khadasevich <nataliabizunkova@gmail.com>
2024-02-20 00:32:40 +0300

    add methods sum,subtract, multiply, divide for double

--- src/main/java/it/academy/nataliakhodasevich/Calculator.java
+++ src/main/java/it/academy/nataliakhodasevich/Calculator.java
@@ -2,19 +2,35 @@ package it.academy.nataliakhodasevich;
 
 public class Calculator {
 
-    public int sum(int a, int b) {
+    public static int sum(int a, int b) {
         return a + b;
     }
 
-    public int subtract(int a, int b) {
+    public static int subtract(int a, int b) {
         return a - b;
     }
 
-    public int multiply(int a, int b) {
+    public static int multiply(int a, int b) {
         return a * b;
     }
 
-    public int divide(int a, int b) {
+    public static int divide(int a, int b) {
+        return a / b;
+    }
+
+    public static double sum(double a, double b) {
+        return a + b;
+    }
+
+    public static double subtract(double a, double b) {
+        return a - b;
+    }
+
+    public static double multiply(double a, double b) {
+        return a * b;
+    }
+
+    public static double divide(double a, double b) {
         return a / b;
     }
 }
