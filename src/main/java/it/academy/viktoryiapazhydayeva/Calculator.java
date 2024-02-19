c2212eaccec383f4296f787b71396f4b84174cc8
viktoryiapazhydayeva <viktoryia_pazhydayeva@epam.com>
2024-02-19 23:35:16 +0300

    Add method sumIntegers

--- src/main/java/it/academy/viktoryiapazhydayeva/Calculator.java
+++ src/main/java/it/academy/viktoryiapazhydayeva/Calculator.java
@@ -1,4 +1,10 @@
 package it.academy.viktoryiapazhydayeva;
 
 public class Calculator {
+    public static int sumIntegers(int a, int b) {
+        int maxInteger = Integer.MAX_VALUE;
+        int sumResultIntegers;
+        sumResultIntegers = a+ b;
+        return sumResultIntegers;
+    }
 }
