d1b5885939182dbe70c6c47f64ecc0447b818280
Yuliya Abramovitch <yuabramovitch@gmail.com>
2024-02-19 23:33:28 +0300

    Add Methods calculateSubtraction, calculateMultiplication, calculateDividing

--- src/main/java/it/academy/yuliyaabramovitch/Calculator.java
+++ src/main/java/it/academy/yuliyaabramovitch/Calculator.java
@@ -1,6 +1,9 @@
 package it.academy.yuliyaabramovitch;
 
+import net.bytebuddy.implementation.bytecode.Multiplication;
+
 public class Calculator {
+    //    ***Sum
     public int calculateSum(int valueA, int valueB) {
         return valueA + valueB;
     }
@@ -18,6 +21,59 @@ public class Calculator {
     }
 
     public String calculateSum(String valueA, String valueB) {
-        return valueA + valueB;
+        return String.join(" ", valueA, valueB);
+    }
+//    ***Subtraction
+
+    public int calculateSubtraction(int valueA, int valueB) {
+        return valueA - valueB;
+    }
+
+    public double calculateSubtraction(double valueA, double valueB) {
+        return valueA - valueB;
+    }
+
+    public double calculateSubtraction(int valueA, double valueB) {
+        return valueA - valueB;
+    }
+
+    public double calculateSubtraction(double valueA, int valueB) {
+        return valueA - valueB;
+    }
+
+    public String calculateSubtraction(String valueA, String valueB) {
+        return "Нельзя выполнить вычитание строк!";
+    }
+//    ***Multiplication
+    public int calculateMultiplication(int valueA, int valueB) {
+        return valueA * valueB;
+    }
+    public double calculateMultiplication(double valueA, double valueB) {
+        return valueA * valueB;
+    }
+    public double calculateMultiplication(int valueA, double valueB) {
+        return valueA * valueB;
+    }
+    public double calculateMultiplication(double valueA, int valueB) {
+        return valueA * valueB;
+    }
+    public String calculateMultiplication(String valueA, String valueB) {
+        return "Нельзя выполнить умножение строк!";
+    }
+//    ***Dividing
+    public int calculateDividing(int valueA, int valueB) {
+        return valueA / valueB;
+    }
+    public double calculateDividing(double valueA, double valueB) {
+        return valueA / valueB;
+    }
+    public double calculateDividing(int valueA, double valueB) {
+        return valueA / valueB;
+    }
+    public double calculateDividing(double valueA, int valueB) {
+        return valueA / valueB;
+    }
+    public String calculateDividing(String valueA, String valueB) {
+        return "Нельзя выполнить деление строк!";
     }
 }
