88d7eb016a7473c856a962dc1d0cd4892d4449b8
Yuliya Abramovitch <yuabramovitch@gmail.com>
2024-02-19 22:27:01 +0300

    Add calculateSum Methods

--- src/main/java/it/academy/yuliyaabramovitch/Calculator.java
+++ src/main/java/it/academy/yuliyaabramovitch/Calculator.java
@@ -1,4 +1,23 @@
 package it.academy.yuliyaabramovitch;
 
 public class Calculator {
+    public int calculateSum(int valueA, int valueB) {
+        return valueA + valueB;
+    }
+
+    public double calculateSum(double valueA, double valueB) {
+        return valueA + valueB;
+    }
+
+    public double calculateSum(int valueA, double valueB) {
+        return valueA + valueB;
+    }
+
+    public double calculateSum(double valueA, int valueB) {
+        return valueA + valueB;
+    }
+
+    public String calculateSum(String valueA, String valueB) {
+        return valueA + valueB;
+    }
 }
