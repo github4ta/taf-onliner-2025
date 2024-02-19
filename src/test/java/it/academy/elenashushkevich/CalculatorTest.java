eb8f9c709606950b98aa9022e7629b82b9e4445d
Alena Shushkevich <elenashushkevich7@gmail.com>
2024-02-19 22:40:44 +0300

    Add add tests

---
+++ src/test/java/it/academy/elenashushkevich/CalculatorTest.java
@@ -0,0 +1,30 @@
+package it.academy.elenashushkevich;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest {
+
+    Calculator calc = new Calculator();
+
+    @Test
+    public void testIntAddZero(){
+        Assertions.assertEquals(0, calc.add(0,0));
+    }
+
+    @Test
+    public void testIntAddMax(){
+        Assertions.assertEquals(-2, calc.add(2147483647,2147483647));
+    }
+
+    @Test
+    public void testIntAddMin(){
+        Assertions.assertEquals(0, calc.add(-2147483648,-2147483648));
+    }
+
+    @Test
+    public void testIntAddMaxMin(){
+        Assertions.assertEquals(-1, calc.add(-2147483648,2147483647));
+    }
+
+}
