4017f501c43e78ce9947b0d97cb19f4e5e5c8f29
Victoria V <a5glyk@gmail.com>
2024-02-20 02:39:06 +0300

    add sum method

--- src/main/java/it/academy/victoriavlasova/Calculator.java
+++ src/main/java/it/academy/victoriavlasova/Calculator.java
@@ -1,4 +1,20 @@
 package it.academy.victoriavlasova;
 
 public class Calculator {
+    public int sum(int a, int b){
+        return a+b;
+    }
+    public double sum(double a, double b){
+        return a+b;
+    }
+    public double sum(int a, double b){
+        return ((double)a)+b;
+    }
+    public double sum(double a, int b){
+        return a+((double)b);
+    }
+    public String sum(String a, String b){
+        return a+b;
+    }
+
 }
