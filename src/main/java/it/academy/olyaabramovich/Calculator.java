5ae58217e1aed787eb13ac23fb43bfcc4d6e84f8
DESKTOP-I2RB543\sweet <olga.abramovich9@gmail.com>
2024-02-19 22:52:47 +0300

    - add calculateSubtraction method; - add calculateMultiplication method; - add calculateDivision method;

--- src/main/java/it/academy/olyaabramovich/Calculator.java
+++ src/main/java/it/academy/olyaabramovich/Calculator.java
@@ -21,4 +21,52 @@ public class Calculator {
     public String calculateSum(String str1, String str2) {
         return str1 + str2;
     }
+
+    public int calculateSubtraction(int a, int b) {
+        return a - b;
+    }
+
+    public double calculateSubtraction(double a, double b) {
+        return a - b;
+    }
+
+    public double calculateSubtraction(int a, double b) {
+        return a - b;
+    }
+
+    public double calculateSubtraction(double a, int b) {
+        return a - b;
+    }
+
+    public int calculateMultiplication(int a, int b) {
+        return a * b;
+    }
+
+    public double calculateMultiplication(double a, double b) {
+        return a * b;
+    }
+
+    public double calculateMultiplication(int a, double b) {
+        return a * b;
+    }
+
+    public double calculateMultiplication(double a, int b) {
+        return a * b;
+    }
+
+    public int calculateDivision(int a, int b) {
+        return a / b;
+    }
+
+    public double calculateDivision(double a, double b) {
+        return a / b;
+    }
+
+    public double calculateDivision(int a, double b) {
+        return a / b;
+    }
+
+    public double calculateDivision(double a, int b) {
+        return a / b;
+    }
 }
