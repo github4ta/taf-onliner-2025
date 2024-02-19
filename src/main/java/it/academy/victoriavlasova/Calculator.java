0be02c64491d61d7c428486230e1fcb9cb174085
Victoria V <a5glyk@gmail.com>
2024-02-20 02:57:50 +0300

    add subtraction method

--- src/main/java/it/academy/victoriavlasova/Calculator.java
+++ src/main/java/it/academy/victoriavlasova/Calculator.java
@@ -16,5 +16,22 @@ public class Calculator {
     public String sum(String a, String b){
         return a+b;
     }
+    public int subtraction(int a, int b){
+        return a-b;
+    }
+    public double subtraction(double a, double b){
+        return a-b;
+    }
+    public double subtraction(int a, double b){
+        return ((double)a)-b;
+    }
+    public double subtraction(double a, int b){
+        return a-((double)b);
+    }
+    public String subtraction(String a, String b){
+        return "?????????? ???????? ?????? ???????? ? ????????? ????? ??????";
+    }
+
+
 
 }
