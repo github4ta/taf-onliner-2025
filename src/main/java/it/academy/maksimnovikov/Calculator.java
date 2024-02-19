c2879ebbb45fe4ebe80b29fa8a1ecb88cc3a9fcd
Maksim Novikov <moksemnovikov@gmail.com>
2024-02-19 23:53:31 +0300

    add sum methods in Calculator

--- src/main/java/it/academy/maksimnovikov/Calculator.java
+++ src/main/java/it/academy/maksimnovikov/Calculator.java
@@ -1,4 +1,44 @@
 package it.academy.maksimnovikov;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
 public class Calculator {
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+
+    public static double sum(double a, double b) {
+        BigDecimal numA = new BigDecimal(a);
+        BigDecimal numB = new BigDecimal(b);
+        return (numA.add(numB)).doubleValue();
+    }
+
+    public static double sum(int a, double b) {
+        BigDecimal numA = new BigDecimal(a);
+        BigDecimal numB = new BigDecimal(b);
+        return (numA.add(numB)).doubleValue();
+    }
+
+    public static double sum(double a, int b) {
+        BigDecimal numA = new BigDecimal(a);
+        BigDecimal numB = new BigDecimal(b);
+        return (numA.add(numB)).doubleValue();
+    }
+
+    public static String sum(String a, String b) {
+        String errorMessage = "Enter the correct number";
+        String regex = "[0-9.-]+";
+        if (a.matches(regex) && b.matches(regex)) {
+            if (a.compareTo(".") > 0 || b.compareTo(".") > 0) {
+                BigDecimal numA = new BigDecimal(a);
+                BigDecimal numB = new BigDecimal(b);
+                return (numA.add(numB)).toString();
+            } else {
+                BigInteger numC = new BigInteger(a);
+                BigInteger numD = new BigInteger(b);
+                return (numC.add(numD)).toString();
+            }
+        } else return errorMessage;
+    }
 }
