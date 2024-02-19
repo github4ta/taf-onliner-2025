f95af45a68a5c0f596eb0059c425567127af5315
Maksim Novikov <moksemnovikov@gmail.com>
2024-02-20 01:42:49 +0300

    add subtract methods in Calculator

--- src/main/java/it/academy/maksimnovikov/Calculator.java
+++ src/main/java/it/academy/maksimnovikov/Calculator.java
@@ -41,4 +41,41 @@ public class Calculator {
             }
         } else return errorMessage;
     }
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public static double subtract(double a, double b) {
+        BigDecimal numA = new BigDecimal(a);
+        BigDecimal numB = new BigDecimal(b);
+        return (numA.subtract(numB)).doubleValue();
+    }
+
+    public static double subtract(int a, double b) {
+        BigDecimal numA = new BigDecimal(a);
+        BigDecimal numB = new BigDecimal(b);
+        return (numA.subtract(numB)).doubleValue();
+    }
+
+    public static double subtract(double a, int b) {
+        BigDecimal numA = new BigDecimal(a);
+        BigDecimal numB = new BigDecimal(b);
+        return (numA.subtract(numB)).doubleValue();
+    }
+
+    public static String subtract(String a, String b) {
+        String errorMessage = "Enter the correct number";
+        String regex = "[0-9.-]+";
+        if (a.matches(regex) && b.matches(regex)) {
+            if (a.indexOf(".") > 0 || b.indexOf(".") > 0) {
+                BigDecimal numA = new BigDecimal(a);
+                BigDecimal numB = new BigDecimal(b);
+                return (numA.subtract(numB)).toString();
+            } else {
+                BigInteger numC = new BigInteger(a);
+                BigInteger numD = new BigInteger(b);
+                return (numC.subtract(numD)).toString();
+            }
+        } else return errorMessage;
+    }
 }
