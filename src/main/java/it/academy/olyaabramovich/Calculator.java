ce4b1d060811c14d6be9169743e17bbb76826893
DESKTOP-I2RB543\sweet <olga.abramovich9@gmail.com>
2024-02-19 22:26:44 +0300

    add calculateSum method.

---
+++ src/main/java/it/academy/olyaabramovich/Calculator.java
@@ -0,0 +1,24 @@
+package it.academy.olyaabramovich;
+
+public class Calculator {
+
+    public int calculateSum(int a, int b) {
+        return a + b;
+    }
+
+    public double calculateSum(double a, double b) {
+        return a + b;
+    }
+
+    public double calculateSum(int a, double b) {
+        return a + b;
+    }
+
+    public double calculateSum(double a, int b) {
+        return a + b;
+    }
+
+    public String calculateSum(String str1, String str2) {
+        return str1 + str2;
+    }
+}
