464c6cae66c209b3f158fb63a96236c4d27c8c50
AntonDomenikov <a.domenikov@gmail.com>
2024-02-20 20:35:56 +0300

    1. Add Main and MainTest classes. 2. Add 2 methods to the CalculatorTest class for checking positive and negative overflow for INT values.

---
+++ src/main/java/it/academy/antondomenikov/Main.java
@@ -0,0 +1,4 @@
+package it.academy.antondomenikov;
+
+public class Main {
+}
--- src/test/java/it/academy/antondomenikov/CalculatorTest.java
+++ src/test/java/it/academy/antondomenikov/CalculatorTest.java
@@ -1,34 +1,54 @@
 package it.academy.antondomenikov;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CalculatorTest {
+
+    private static Calculator calculator;
+
+    @BeforeAll
+    public static void setUpCalculator() {
+        calculator = new Calculator();
+    }
+
     @Test
     public void testAddIntIntPositiveNumbers() {
-        Calculator calculator = new Calculator();
         int result = calculator.addIntInt(3, 4);
         assertEquals(7, result);
     }
 
     @Test
     public void testAddIntIntNegativeNumbers() {
-        Calculator calculator = new Calculator();
         int result = calculator.addIntInt(-3, -4);
         assertEquals(-7, result);
     }
 
     @Test
     public void testAddIntIntZeroAndPositive() {
-        Calculator calculator = new Calculator();
         int result = calculator.addIntInt(0, 5);
         assertEquals(5, result);
     }
 
     @Test
     public void testAddIntIntZeroAndNegative() {
-        Calculator calculator = new Calculator();
         int result = calculator.addIntInt(0, -5);
         assertEquals(-5, result);
     }
+
+    @Test
+    public void testAddIntIntPositiveOverflow() {
+        int result = calculator.addIntInt(Integer.MAX_VALUE, 1);
+        assertEquals(Integer.MIN_VALUE, result);
+    }
+
+    @Test
+    public void testAddIntIntNegativeOverflow() {
+        int result = calculator.addIntInt(Integer.MIN_VALUE, -1);
+        assertEquals(Integer.MAX_VALUE, result);
+
+    }
+
 }
