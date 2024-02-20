86356cea639585e8043a64ac32a3e938f0233d67
n.khadasevich <nataliabizunkova@gmail.com>
2024-02-20 11:13:14 +0300

    add tests for int

--- src/test/java/it/academy/nataliakhodasevich/CalculatorTest.java
+++ src/test/java/it/academy/nataliakhodasevich/CalculatorTest.java
@@ -1,7 +1,33 @@
 package it.academy.nataliakhodasevich;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class CalculatorTest {
+    @Test
+    public void testSumIntPositiveNegative(){
+        Assertions.assertEquals(0,Calculator.sum(-1,1));
+    }
+
+    @Test
+    public void testSumIntNegativeNegative(){
+        Assertions.assertEquals(-2,Calculator.sum(-1,-1));
+    }
+
+    @Test
+    public void testSumIntPositivePositive(){
+        Assertions.assertEquals(2,Calculator.sum(1,1));
+    }
+
+    @Test
+    public void testSumIntPositiveZero(){
+        Assertions.assertEquals(1,Calculator.sum(1,0));
+    }
+
+    @Test
+    public void testSumIntNegativeZero(){
+        Assertions.assertEquals(-1,Calculator.sum(-1,0));
+    }
+
 
 }
