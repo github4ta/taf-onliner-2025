4a1f90f51292467501654e25052529a9c5d3df96
daryaPadaliak <darya.padaliak@gmail.com>
2024-02-20 20:12:50 +0100

    add sum, subtract, division, multiply methods for int/double

--- src/main/java/by/itacademy/padaliak/Calculator.java
+++ src/main/java/by/itacademy/padaliak/Calculator.java
@@ -1,4 +1,67 @@
 package by.itacademy.padaliak;
 
 public class Calculator {
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+
+    public static double sum(double a, double b) {
+        return a + b;
+    }
+
+    public static double sum(int a, double b) {
+        return a + b;
+    }
+
+    public static double sum(double a, int b) {
+        return a + b;
+    }
+
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public static double subtract(double a, double b) {
+        return a - b;
+    }
+
+    public static double subtract(int a, double b) {
+        return a - b;
+    }
+
+    public static double subtract(double a, int b) {
+        return a - b;
+    }
+
+    public static int division(int a, int b) {
+        return a / b;
+    }
+
+    public static double division(double a, double b) {
+        return a / b;
+    }
+
+    public static double division(int a, double b) {
+        return a / b;
+    }
+
+    public static double division(double a, int b) {
+        return a / b;
+    }
+
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public static double multiply(double a, double b) {
+        return a * b;
+    }
+
+    public static double multiply(int a, double b) {
+        return a * b;
+    }
+
+    public static double multiply(double a, int b) {
+        return a * b;
+    }
 }
