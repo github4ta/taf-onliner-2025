77f70aee3a8212cfee2a196b21610c401280d198
daryaPadaliak <darya.padaliak@gmail.com>
2024-02-20 21:03:49 +0100

    add subtract tests for int

--- src/test/java/by/itacademy/padaliak/CalculatorTest.java
+++ src/test/java/by/itacademy/padaliak/CalculatorTest.java
@@ -70,4 +70,82 @@ public class CalculatorTest {
     public void test11() {
         Assertions.assertEquals(-2147483648, Calculator.sum(-2147483648, 0));
     }
+
+    @Test
+    @DisplayName("int: positive - positive")
+    public void test12() {
+        Assertions.assertEquals(17, Calculator.subtract(20, 3));
+    }
+
+    @Test
+    @DisplayName("int: negative - negative")
+    public void test13() {
+        Assertions.assertEquals(-17, Calculator.subtract(-20, -3));
+    }
+
+    @Test
+    @DisplayName("int: positive - negative")
+    public void test14() {
+        Assertions.assertEquals(23, Calculator.subtract(20, -3));
+    }
+
+    @Test
+    @DisplayName("int: negative - positive")
+    public void test15() {
+        Assertions.assertEquals(-67, Calculator.subtract(-20, 47));
+    }
+
+    @Test
+    @DisplayName("int: zero - zero")
+    public void test16() {
+        Assertions.assertEquals(0, Calculator.subtract(0, 0));
+    }
+
+    @Test
+    @DisplayName("int: zero - positive")
+    public void test17() {
+        Assertions.assertEquals(-67, Calculator.subtract(0, 67));
+    }
+
+    @Test
+    @DisplayName("int: zero - negative")
+    public void test18() {
+        Assertions.assertEquals(67, Calculator.subtract(0, -67));
+    }
+
+    @Test
+    @DisplayName("int: positive - zero")
+    public void test19() {
+        Assertions.assertEquals(78, Calculator.subtract(78, 0));
+    }
+
+    @Test
+    @DisplayName("int: negative - zero")
+    public void test20() {
+        Assertions.assertEquals(-78, Calculator.subtract(-78, 0));
+    }
+
+    @Test
+    @DisplayName("int: 2147483647 - zero")
+    public void test21() {
+        Assertions.assertEquals(2147483647, Calculator.subtract(2147483647, 0));
+    }
+
+    @Test
+    @DisplayName("int: 2147483647 - 1")
+    public void test22() {
+        Assertions.assertEquals(2147483646, Calculator.subtract(2147483647, 1));
+    }
+
+    @Test
+    @DisplayName("int: -2147483648 - zero")
+    public void test23() {
+        Assertions.assertEquals(-2147483648, Calculator.subtract(-2147483648, 0));
+    }
+
+    @Test
+    @DisplayName("int: -2147483648 - 1")
+    public void test24() {
+        Assertions.assertEquals(-2147483647, Calculator.subtract(-2147483648, -1));
+    }
 }
