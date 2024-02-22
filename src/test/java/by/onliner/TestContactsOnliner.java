aee6253e50442ad476cea7c2d7dc1f5b97deba9d
Alena Shushkevich <elenashushkevich7@gmail.com>
2024-02-22 23:28:05 +0300

    add methods and beforeTest

--- src/test/java/by/onliner/TestContactsOnliner.java
+++ src/test/java/by/onliner/TestContactsOnliner.java
@@ -3,13 +3,27 @@ package by.onliner;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TestContactsOnliner {
+    String redactionContactsXpath = "//*[contains(text(),\"Контакты редакции\")]";
+    String titleContactsPageXpath = "//div[@class=\"news-header__title\"]";
     ChromeDriver driver;
 
+    public void navigateToContacts() {
+        WebElement navigateToContacts = driver.findElement(By.xpath(redactionContactsXpath));
+        navigateToContacts.click();
+    }
+
+    public String titleContactsPage() {
+        WebElement titleContactsPage = driver.findElement(By.xpath(titleContactsPageXpath));
+        titleContactsPage.getText();
+    }
+
     @BeforeEach
-    public  void beforeTest() {
+    public void beforeTest() {
         driver = new ChromeDriver();
         String url = "https://www.onliner.by/";
         driver.manage().window().maximize();
@@ -17,8 +31,5 @@ public class TestContactsOnliner {
     }
 
     @Test
-    public 
-
-
-
+    public
 }
