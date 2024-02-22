15c440ba174491dd050b9e673e1efcf9d5c58ef4
Victoria V <a5glyk@gmail.com>
2024-02-23 01:42:41 +0300

    add BeforeEach and AfterEach methods

--- src/test/java/by/onliner/AutobaraholkaTest.java
+++ src/test/java/by/onliner/AutobaraholkaTest.java
@@ -1,22 +1,31 @@
 package by.onliner;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class AutobaraholkaTest {
-    @Test
-    public void testOpenAutobaraholka(){
-        WebDriver driver = new ChromeDriver();
+    WebDriver driver;
+    @BeforeEach
+    public void startTest(){
+        driver = new ChromeDriver();
         driver.manage().window().maximize();
         String url ="https://www.onliner.by/";
         driver.navigate().to(url);
+    }
+    @Test
+    public void testOpenAutobaraholka(){
         String autobaraholkaXpath = "//ul[@class='b-main-navigation']/li[3]/a";
         driver.findElement(By.xpath(autobaraholkaXpath)).click();
         String actualTitleXpath = "//h1[@class='vehicle-form__title vehicle-form__title_big-alter']";
-        Assertions.assertEquals("?????????????", driver.findElement(By.xpath(actualTitleXpath)).getText());
+        Assertions.assertEquals("Автобарахолка", driver.findElement(By.xpath(actualTitleXpath)).getText());
+    }
+    @AfterEach
+    public void finishTest(){
         driver.quit();
     }
 }
