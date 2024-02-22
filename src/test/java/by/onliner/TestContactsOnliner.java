263105a0bfdf36b31badc61342a356b00d1d7e0b
Alena Shushkevich <elenashushkevich7@gmail.com>
2024-02-22 23:46:20 +0300

    add wait

--- src/test/java/by/onliner/TestContactsOnliner.java
+++ src/test/java/by/onliner/TestContactsOnliner.java
@@ -1,35 +1,31 @@
 package by.onliner;
 
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class TestContactsOnliner {
-    String redactionContactsXpath = "//*[contains(text(),\"Контакты редакции\")]";
-    String titleContactsPageXpath = "//div[@class=\"news-header__title\"]";
-    ChromeDriver driver;
-
-    public void navigateToContacts() {
-        WebElement navigateToContacts = driver.findElement(By.xpath(redactionContactsXpath));
-        navigateToContacts.click();
-    }
-
-    public String titleContactsPage() {
-        WebElement titleContactsPage = driver.findElement(By.xpath(titleContactsPageXpath));
-        titleContactsPage.getText();
-    }
-
-    @BeforeEach
-    public void beforeTest() {
-        driver = new ChromeDriver();
-        String url = "https://www.onliner.by/";
-        driver.manage().window().maximize();
-        driver.navigate().to(url);
-    }
-
     @Test
-    public
+    public void onlinerContactsTest() {
+
+        ChromeDriver chromeDriver = new ChromeDriver();
+        String url = "https://www.onliner.by/";
+        chromeDriver.manage().window().maximize();
+        chromeDriver.navigate().to(url);
+        String redactionContactsXpath = "//*[contains(text(),\"Контакты редакции\")]";
+        String titleContactsPageXpath = "//div[@class=\"news-header__title\"]";
+
+        WebDriverWait wait = new WebDriverWait(chromeDriver, Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(redactionContactsXpath))).click();
+
+
+        WebElement titleContactsPage = chromeDriver.findElement(By.xpath(titleContactsPageXpath));
+        Assertions.assertTrue(titleContactsPage.getText().contains("Контакты редакции."));
+    }
 }
