ff3f2f6750afd07df1e4d9703b8d485de0bea8ad
daryaPadaliak <darya.padaliak@gmail.com>
2024-02-22 20:05:24 +0100

    att testOnliner method

--- src/test/java/by/onliner/OnlinerTest.java
+++ src/test/java/by/onliner/OnlinerTest.java
@@ -1,4 +1,13 @@
 package by.onliner;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class OnlinerTest {
+    @Test
+    public void testOnliner() {
+    }
 }
