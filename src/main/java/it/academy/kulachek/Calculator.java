73ecfe60c13c7f0a67d256dda5f40bb7e30576f5
Alyona-Kulachek <kulachek91@inbox.ru>
2024-02-22 19:52:03 +0700

    add sumNumbers method

--- src/main/java/it/academy/kulachek/Calculator.java
+++ src/main/java/it/academy/kulachek/Calculator.java
@@ -1,4 +1,20 @@
 package it.academy.kulachek;
 
 public class Calculator {
+    public int sumNumbers(int a, int b) {
+        return a + b;
+    }
+
+    public int subtractNumbers(int a, int b) {
+        return a - b;
+    }
+
+    public int multiplyNumbers(int a, int b) {
+        return a * b;
+    }
+
+    public int divideNumbers(int a, int b) {
+        return a / b;
+    }
+
 }
