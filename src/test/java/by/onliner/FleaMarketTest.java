510ef80e8720aa5ae4538086090a97a356ca1d16
Maksim Novikov <moksemnovikov@gmail.com>
2024-02-22 12:55:39 +0300

    add test FleaMarket

--- src/test/java/by/onliner/FleaMarketTest.java
+++ src/test/java/by/onliner/FleaMarketTest.java
@@ -1,4 +1,21 @@
 package by.onliner;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class FleaMarketTest {
+    @Test
+    public void testFleaMarket(){
+        ChromeDriver driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        final String URL="https://www.onliner.by/";
+        driver.get(URL);
+        final By FLEA_MARKET_BUTTON_XPATH=By.xpath("//a[@href=\"https://baraholka.onliner.by/\"]");
+        final By HEADING_XPATH=By.xpath("//h1[@class=\"m-title-i\"]");
+        driver.findElement(FLEA_MARKET_BUTTON_XPATH).click();
+        Assertions.assertEquals("Барахолка",driver.findElement(HEADING_XPATH).getText());
+        driver.quit();
+    }
 }
