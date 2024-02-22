b8a9c6deff8bc5a60dd3038ab31b6d2969936669
n.khadasevich <nataliabizunkova@gmail.com>
2024-02-22 10:01:46 +0300

    add driver settings

--- src/test/java/by/onliner/JobsTest.java
+++ src/test/java/by/onliner/JobsTest.java
@@ -1,4 +1,15 @@
 package by.onliner;
 
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class JobsTest {
+    @Test
+    public void testJobs() {
+        ChromeDriver driver= new ChromeDriver();
+        driver.manage().window().maximize();
+        String url="https://www.onliner.by/";
+        driver.navigate().to(url);
+
+    }
 }
