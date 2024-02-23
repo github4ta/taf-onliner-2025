4f23b525f0905655cd809ed64d271799bcbe5ed9
Natalia Gvozdeva (intern) <gvozdeva-natalia@mail.ru>
2024-02-23 21:38:44 +0300

    Add new class Calculator

---
+++ src/main/java/it/academy/nataliagvozdeva/Calculator.java
@@ -0,0 +1,18 @@
+package it.academy.nataliagvozdeva;
+
+public class Calculator {
+
+    public int sum(int a, int b){
+        return a + b;
+    }
+    public int subtraction(int a, int b){
+        return a - b;
+    }
+    public int division(int a, int b){
+        return a / b;
+    }
+    public int multiplication(int a, int b){
+        return a * b;
+    }
+
+}
