0467477627a81944004b3cfe2aba02af8d39a774
Alena Shushkevich <elenashushkevich7@gmail.com>
2024-02-23 09:38:23 +0300

    delete invalid import

--- src/test/java/by/onliner/TestContactsOnliner.java
+++ src/test/java/by/onliner/TestContactsOnliner.java
@@ -3,11 +3,9 @@ package by.onliner;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+
 
 import java.time.Duration;
 
