70eab71d41e129d7951a86569f56b20f1b66382c
Alyona-Kulachek <kulachek91@inbox.ru>
2024-02-23 21:06:33 +0700

    add locators

--- src/test/java/it/academy/kulachek/Test.java
+++ src/test/java/it/academy/kulachek/Test.java
@@ -1,9 +1,17 @@
 package it.academy.kulachek;
 
+import org.junit.jupiter.api.Assertions;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Test {
     WebDriver driver = new ChromeDriver();
+    static String url = "https://www.onliner.by/";
+    static String sectionServices = "(//a[@href='https://s.onliner.by/tasks'])[2]";
+    static String titleOrders = "//div[@class='service-header__title service-header__title_huge']";
+
+
 
 }
