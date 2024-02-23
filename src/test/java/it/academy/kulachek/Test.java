a46e1b63c544a6da49bc7311d8a6fc53ebeab697
Alyona-Kulachek <kulachek91@inbox.ru>
2024-02-23 20:53:30 +0700

    add WebDriver driver

--- src/test/java/it/academy/kulachek/Test.java
+++ src/test/java/it/academy/kulachek/Test.java
@@ -1,5 +1,9 @@
 package it.academy.kulachek;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Test {
+    WebDriver driver = new ChromeDriver();
 
 }
