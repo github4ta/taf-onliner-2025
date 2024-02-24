4a1410209c2cba1491a64f954825ec1978166305
Yuliya Abramovitch <yuabramovitch@gmail.com>
2024-02-24 20:01:24 +0300

    Add code to DemoOne, Add class DemoTwo

--- src/main/java/DemoOne.java
+++ src/main/java/DemoOne.java
@@ -1,7 +1,12 @@
+import by.onliner.DemoTwo;
+
 public class DemoOne {
     public static void main(String[] args) {
         System.out.println("Hello, master!");
         System.out.println(Math.max(1, 9));
         System.out.println("Hello, java!");
+        DemoTwo demoTwo=new DemoTwo();
+        String unitNumber=demoTwo.setUnitNumber("UI-999");
+        demoTwo.printText(unitNumber);
     }
 }
