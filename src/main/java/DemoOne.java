f4f7cc1e086174e7a05fa7dcb72d2b526bb902dc
Alena Shushkevich <elenashushkevich7@gmail.com>
2024-02-24 20:17:53 +0300

    add new

--- src/main/java/DemoOne.java
+++ src/main/java/DemoOne.java
@@ -1,16 +1,16 @@
-public class DemoOne {
+public class DemoOne extends Demo2907 {
     public static void main(String[] args) {
         System.out.println("Hello, master!");
         System.out.println("Hello, java!");
 
         String str1 = "onliner";
         String str2 = "my_onlinER_test";
+        System.out.println(DemoOne.name + " " + DemoOne.surname + " " + DemoOne.age);
 
         if (str2.toLowerCase().contains(str1)) {
             System.out.println("Строка 1 входит в строку 2");
         } else {
             System.out.println("Совпадений нет.");
         }
-        System.out.println(DemoOne.name+" "+DemoOne.surname+" " +DemoOne.age);
     }
 }
