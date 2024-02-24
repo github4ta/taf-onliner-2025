e62544f3a438dea3459c521e973541c2fe5195da
n.khadasevich <nataliabizunkova@gmail.com>
2024-02-24 20:22:56 +0300

    add code to DemoSuperOne

---
+++ src/main/java/DemoSuperOneNatKhodasevich.java
@@ -0,0 +1,7 @@
+public class DemoSuperOneNatKhodasevich {
+    static String str="Hello";
+    public static void main(String[] args) {
+        System.out.println("Hi Team!");
+        System.out.println(str);
+    }
+}
