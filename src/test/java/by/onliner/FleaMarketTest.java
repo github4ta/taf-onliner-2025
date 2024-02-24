50d199ab4a0098435ec56a8cc6754327db3d1f77
Maksim Novikov <moksemnovikov@gmail.com>
2024-02-24 21:20:48 +0300

    FleaMarketTest extends BaseTest

--- src/test/java/by/onliner/FleaMarketTest.java
+++ src/test/java/by/onliner/FleaMarketTest.java
@@ -5,17 +5,12 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class FleaMarketTest {
+public class FleaMarketTest extends BaseTest {
     @Test
-    public void testFleaMarket(){
-        ChromeDriver driver=new ChromeDriver();
-        driver.manage().window().maximize();
-        final String URL="https://www.onliner.by/";
-        driver.get(URL);
-        final By FLEA_MARKET_BUTTON_XPATH=By.xpath("//a[@href=\"https://baraholka.onliner.by/\"]");
-        final By HEADING_XPATH=By.xpath("//h1[@class=\"m-title-i\"]");
+    public void testFleaMarket() {
+        final By FLEA_MARKET_BUTTON_XPATH = By.xpath("//a[@href=\"https://baraholka.onliner.by/\"]");
+        final By HEADING_XPATH = By.xpath("//h1[@class=\"m-title-i\"]");
         driver.findElement(FLEA_MARKET_BUTTON_XPATH).click();
-        Assertions.assertEquals("Барахолка",driver.findElement(HEADING_XPATH).getText());
-        driver.quit();
+        Assertions.assertEquals("Барахолка", driver.findElement(HEADING_XPATH).getText());
     }
 }
