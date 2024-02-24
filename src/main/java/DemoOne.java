836f99b39a0f1013ddff3ff5587b9bc884a37d8c
Natalia Gvozdeva (intern) <gvozdeva-natalia@mail.ru>
2024-02-24 19:06:07 +0300

    Add main method

--- src/main/java/DemoOne.java
+++ src/main/java/DemoOne.java
@@ -1,18 +1,5 @@
 public class DemoOne {
-
-
-
-    static public void main(String    [] args) {
-
-
-
-        System.out.println("Hello, master!")         ;
-
-
+    public static void main(String[] args) {
+        System.out.println("Hello, master!");
     }
 }
-
-
-
-
-
