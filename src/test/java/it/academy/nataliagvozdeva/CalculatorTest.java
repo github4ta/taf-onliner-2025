3402c03f506df2765d4044e1ddf4961ce3603813
Natalia Gvozdeva (intern) <gvozdeva-natalia@mail.ru>
2024-02-24 15:52:26 +0300

    Added lines in CalculatorTest class

--- src/test/java/it/academy/nataliagvozdeva/CalculatorTest.java
+++ src/test/java/it/academy/nataliagvozdeva/CalculatorTest.java
@@ -6,83 +6,102 @@ import org.junit.jupiter.api.Test;
 public class CalculatorTest {
 
     @Test
-    public void testSumIntAIntB(){
+    public void testSumIntAIntB() {
         Assertions.assertEquals(11, Calculator.sum(1, 10), "Метод sum работает некорректно.");
     }
+
     @Test
-    public void testSubtractionIntAIntB(){
+    public void testSubtractionIntAIntB() {
         Assertions.assertEquals(-9, Calculator.subtraction(1, 10), "Метод subtraction работает некорректно.");
     }
+
     @Test
-    public void testDivisionIntAIntB(){
+    public void testDivisionIntAIntB() {
         Assertions.assertEquals(1, Calculator.division(5, 5), "Метод division работает некорректно.");
     }
+
     @Test
-    public void testMultiplicationIntAIntB(){
+    public void testMultiplicationIntAIntB() {
         Assertions.assertEquals(25, Calculator.multiplication(5, 5), "Метод multiplication работает некорректно.");
     }
+
     @Test
-    public void testSumDoubleADoubleB(){
+    public void testSumDoubleADoubleB() {
         Assertions.assertEquals(11.6, Calculator.sum(1.5, 10.1), "Метод sum работает некорректно.");
     }
+
     @Test
-    public void testSubtractionDoubleADoubleB(){
+    public void testSubtractionDoubleADoubleB() {
         Assertions.assertEquals(0.0, Calculator.subtraction(10.1, 10.1), "Метод subtraction работает некорректно.");
     }
+
     @Test
-    public void testDivisionDoubleADoubleB(){
+    public void testDivisionDoubleADoubleB() {
         Assertions.assertEquals(1.0, Calculator.division(5.0, 5.0), "Метод division работает некорректно.");
     }
+
     @Test
-    public void testMultiplicationDoubleADoubleB(){
+    public void testMultiplicationDoubleADoubleB() {
         Assertions.assertEquals(25, Calculator.multiplication(5, 5), "Метод multiplication работает некорректно.");
     }
+
     @Test
-    public void testSumIntADoubleB(){
+    public void testSumIntADoubleB() {
         Assertions.assertEquals(6.8, Calculator.sum(1, 5.8), "Метод sum работает некорректно.");
     }
+
     @Test
-    public void testSubtractionIntADoubleB(){
+    public void testSubtractionIntADoubleB() {
         Assertions.assertEquals(90.0, Calculator.subtraction(100, 10.0), "Метод subtraction работает некорректно.");
     }
+
     @Test
-    public void testDivisionIntADoubleB(){
+    public void testDivisionIntADoubleB() {
         Assertions.assertEquals(1.0, Calculator.division(5, 5.0), "Метод division работает некорректно.");
     }
+
     @Test
-    public void testMultiplicationIntADoubleB(){
+    public void testMultiplicationIntADoubleB() {
         Assertions.assertEquals(25.0, Calculator.multiplication(5, 5.0), "Метод multiplication работает некорректно.");
     }
+
     @Test
-    public void testSumDoubleAIntB(){
+    public void testSumDoubleAIntB() {
         Assertions.assertEquals(11.9, Calculator.sum(1.9, 10), "Метод sum работает некорректно.");
     }
+
     @Test
-    public void testSubtractionDoubleAIntB(){
+    public void testSubtractionDoubleAIntB() {
         Assertions.assertEquals(90.0, Calculator.subtraction(100.0, 10), "Метод subtraction работает некорректно.");
     }
+
     @Test
-    public void testDivisionDoubleAIntB(){
+    public void testDivisionDoubleAIntB() {
         Assertions.assertEquals(1.0, Calculator.division(5.0, 5), "Метод division работает некорректно.");
     }
+
     @Test
-    public void testMultiplicationDoubleAIntB(){
+    public void testMultiplicationDoubleAIntB() {
         Assertions.assertEquals(25.0, Calculator.multiplication(5.0, 5), "Метод multiplication работает некорректно.");
     }
+
     @Test
-    public void testSumStringAStringB(){
+    public void testSumStringAStringB() {
         Assertions.assertEquals("108", Calculator.sum("100", "8"), "Метод sum работает некорректно.");
     }
+
     @Test
-    public void testSubtractionStringAStringB(){
+    public void testSubtractionStringAStringB() {
         Assertions.assertEquals("-9", Calculator.subtraction("1", "10"), "Метод subtraction работает некорректно.");
     }
+
     @Test
-    public void testDivisionStringAStringB(){
+    public void testDivisionStringAStringB() {
         Assertions.assertEquals("1", Calculator.division("5", "5"), "Метод division работает некорректно.");
     }
+
     @Test
-    public void testMultiplicationStringAStringB(){
+    public void testMultiplicationStringAStringB() {
         Assertions.assertEquals("25", Calculator.multiplication("5", "5"), "Метод multiplication работает некорректно.");
     }
 }
