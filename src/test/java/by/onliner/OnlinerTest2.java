377c78ccdfa288d07627a03f8acc191a33182035
Alyona-Kulachek <kulachek91@inbox.ru>
2024-02-25 01:27:29 +0700

    add extends BaseTest

--- src/test/java/by/onliner/OnlinerTest2.java
+++ src/test/java/by/onliner/OnlinerTest2.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class OnlinerTest2 {
+public class OnlinerTest2 extends BaseTest{
     @Test
     public void testOnliner2() {
         WebDriver driver = new ChromeDriver();
