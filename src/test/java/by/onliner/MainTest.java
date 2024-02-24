21659690444125e974ae4434da304038bc66e63d
AntonDomenikov <a.domenikov@gmail.com>
2024-02-24 21:32:12 +0300

    Refactor test method

--- src/test/java/by/onliner/MainTest.java
+++ src/test/java/by/onliner/MainTest.java
@@ -5,14 +5,10 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class MainTest {
+public class MainTest extends BaseTest {
 
     @Test
     public void testApartmentsMarket(){
-        ChromeDriver driver=new ChromeDriver();
-        driver.manage().window().maximize();
-        final String URL="https://www.onliner.by/";
-        driver.get(URL);
         final By APARTMENTS_MARKET_BUTTON_XPATH=By.xpath("//a[@href=\"https://r.onliner.by/pk\"]");
         final By HEADING_XPATH=By.xpath("//*[@id=\"container\"]/div/div[2]/div/div/div[1]/div/div[1]/ul/li[1]/a/span/span");
         driver.findElement(APARTMENTS_MARKET_BUTTON_XPATH).click();
