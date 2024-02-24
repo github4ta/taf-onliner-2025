6866a200fe949274e546ea408c33d98da443b4c3
Victoria Vlasova <a5glyk@gmail.com>
2024-02-24 20:02:41 +0300

    refactor system.out.println method

--- src/main/java/DemoOne.java
+++ src/main/java/DemoOne.java
@@ -1,7 +1,5 @@
 public class DemoOne {
     public static void main(String[] args) {
-        System.out.println("Hello, master!");
-        System.out.println(Math.max(1, 9));
-        System.out.println("Hello, java!");
+        System.out.println("New line");
     }
 }
