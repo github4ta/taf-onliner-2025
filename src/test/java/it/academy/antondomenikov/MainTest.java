54191109a814b977f67d7244f3eff07dece9cb2d
AntonDomenikov <a.domenikov@gmail.com>
2024-02-24 18:37:39 +0300

    Removed 1 Test method

--- src/test/java/it/academy/antondomenikov/MainTest.java
+++ src/test/java/it/academy/antondomenikov/MainTest.java
@@ -7,16 +7,4 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class MainTest {
 
-    @Test
-    public void testApartmentsMarket(){
-        ChromeDriver driver=new ChromeDriver();
-        driver.manage().window().maximize();
-        final String URL="https://www.onliner.by/";
-        driver.get(URL);
-        final By APARTMENTS_MARKET_BUTTON_XPATH=By.xpath("//a[@href=\"https://r.onliner.by/pk\"]");
-        final By HEADING_XPATH=By.xpath("//*[@id=\"container\"]/div/div[2]/div/div/div[1]/div/div[1]/ul/li[1]/a/span/span");
-        driver.findElement(APARTMENTS_MARKET_BUTTON_XPATH).click();
-        Assertions.assertEquals("Продажа",driver.findElement(HEADING_XPATH).getText());
-        driver.quit();
-    }
 }
