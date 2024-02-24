144dd648a368fa2709989ec576587e7f30a464d5
AntonDomenikov <a.domenikov@gmail.com>
2024-02-24 20:12:47 +0300

    1. Refactor code from Anton

--- src/main/java/DemoOne.java
+++ src/main/java/DemoOne.java
@@ -1,10 +1,9 @@
-public class DemoOne extends Demo2907 {
+
 public class DemoOne extends DemoSuperOne {
     public static void main(String[] args) {
         System.out.println(name);
         System.out.println("Hello, master!");
         System.out.println(Math.max(1, 9));
         System.out.println("Hello, java!");
-        System.out.println(DemoOne.name+" "+DemoOne.surname+" " +DemoOne.edge);
     }
 }
