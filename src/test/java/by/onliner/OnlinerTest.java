f8b50b071f1960066ecc815fe1a2ded339134ab1
daryaPadaliak <darya.padaliak@gmail.com>
2024-02-25 13:12:00 +0100

    1. delete target 2. refactor OnlinerTest class 3. add ForumTest class

--- src/test/java/by/onliner/OnlinerTest.java
+++ src/test/java/by/onliner/OnlinerTest.java
@@ -34,16 +34,4 @@ public class OnlinerTest extends BaseTest{
         String actualCatalogHeaderText = driver.findElement(By.cssSelector(".catalog-navigation__title")).getText();
         Assertions.assertTrue(actualCatalogHeaderText.contains(expectedCatalogHeaderText));
     }
-    @Test
-        public void testOnliner() {
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.get("https://www.onliner.by/");
-        String forumXpath = "//a[@href='https://forum.onliner.by/']/span[@class='b-main-navigation__text']";
-        String personalDataXpath = "//button[@aria-label='Соглашаюсь']/p[@class='fc-button-label']";
-        String titleXpath = "//h1[@class='m-title']";
-        driver.findElement(By.xpath(forumXpath)).click();
-        driver.findElement(By.xpath(personalDataXpath)).click();
-        Assertions.assertEquals("Форум", driver.findElement(By.xpath(titleXpath)).getText());
-    }
 }
