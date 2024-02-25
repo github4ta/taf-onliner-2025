5dc8913042ff9a496270e90b6b556d9d8ede1971
Yuliya Abramovitch <yuabramovitch@gmail.com>
2024-02-25 17:16:16 +0300

    Refactor CatalogLocator Class

--- src/main/java/by/onliner/pages/HomePageLocator.java
+++ src/main/java/by/onliner/pages/HomePageLocator.java
@@ -5,6 +5,6 @@ import org.openqa.selenium.By;
 public class HomePageLocator {
 
     public static final String LOCATOR = "you locator to the link web element";
-    public static By catalogBtn = By.cssSelector(".b-top-menu a[href='https://catalog.onliner.by']");
+    public static final By CATALOG_BTN = By.cssSelector(".b-top-menu a[href='https://catalog.onliner.by']");
     public static final String AUTOBARAHOLKA_XPATH = "//ul[@class='b-main-navigation']/li[3]/a";
 }
--- src/test/java/by/onliner/CatalogTest.java
+++ src/test/java/by/onliner/CatalogTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 public class CatalogTest extends BaseTest{
 @Test
     public void testCatalogOpened(){
-    driver.findElement(HomePageLocator.catalogBtn).click();
+    driver.findElement(HomePageLocator.CATALOG_BTN).click();
     String expectedCatalogHeaderText = "Каталог";
     String actualCatalogHeaderText = driver.findElement(CatalogLocator.catalogHeader).getText();
     Assertions.assertTrue(actualCatalogHeaderText.contains(expectedCatalogHeaderText));
