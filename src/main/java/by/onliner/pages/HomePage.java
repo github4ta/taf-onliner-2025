01753ff114e6b38005b706b9a700d7a12e328015
daryaPadaliak <darya.padaliak@gmail.com>
2024-02-25 17:26:03 +0100

    1. refactor ForumLocator class 2. refactor ForumTest class 3. refactor HomePage 4. refactor HomePageLocator

--- src/main/java/by/onliner/pages/HomePage.java
+++ src/main/java/by/onliner/pages/HomePage.java
@@ -1,4 +1,23 @@
 package by.onliner.pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
 public class HomePage {
+    WebDriver driver;
+
+    public HomePage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void openForum() {
+        WebElement forumBtn = driver.findElement(By.xpath(HomePageLocator.FORUM_XPATH));
+        forumBtn.click();
+    }
+
+    public void clickPolicy() {
+        WebElement policy = driver.findElement(By.xpath(HomePageLocator.PERSONAL_DATA_XPATH));
+        policy.click();
+    }
 }
