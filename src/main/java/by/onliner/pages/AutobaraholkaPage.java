741b6eca328e7d893cda94dad2fd34a3c3730ec5
Victoria Vlasova <a5glyk@gmail.com>
2024-02-25 19:42:34 +0300

    refactor AutobaraholkaPage class

--- src/main/java/by/onliner/pages/AutobaraholkaPage.java
+++ src/main/java/by/onliner/pages/AutobaraholkaPage.java
@@ -3,13 +3,15 @@ package by.onliner.pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class Autobaraholka {
+public class AutobaraholkaPage {
     WebDriver driver;
 
-    public Autobaraholka(WebDriver driver) {
+    public AutobaraholkaPage(WebDriver driver) {
         this.driver = driver;
     }
-
+    public void clickAutobaraholkaBtn(){
+        driver.findElement(By.xpath(HomePageLocator.AUTOBARAHOLKA_XPATH)).click();
+    }
     public String getAutobaraholkaTitle() {
          return driver.findElement(By.xpath(AutobaraholkaLocator.ACTUAL_TITLE_FOR_AUTOBARAHOLKA_XPATH)).getText();
     }
