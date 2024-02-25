e6cee1a3b5c677bdad4b08ab5913936710476674
viktoryiapazhydayeva <viktoryia_pazhydayeva@epam.com>
2024-02-25 13:20:31 +0300

    Refactor OnlinerTest class. Add XPathes and methods to AboutTest class.

--- src/test/java/by/onliner/AboutTest.java
+++ src/test/java/by/onliner/AboutTest.java
@@ -1,4 +1,22 @@
 package by.onliner;
 
-public class AboutTest {
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class AboutTest extends BaseTest{
+    public static String aboutCompanyLinkXPath = "//a[@href='https://blog.onliner.by/about']";
+    public static String aboutCompanyPageTitleXPath = "//div[@class='news-header__title']/h1";
+
+    public void openAboutCompanyPage() {
+        WebElement aboutCompanyLink = driver.findElement(By.xpath(aboutCompanyLinkXPath));
+        aboutCompanyLink.click();
+    }
+
+    public String getAboutCompanyPageTitleText() {
+        WebElement aboutCompanyPageTitle = driver.findElement(By.xpath(aboutCompanyPageTitleXPath));
+        return aboutCompanyPageTitle.getText();
+    }
 }
--- src/test/java/by/onliner/OnlinerTest.java
+++ src/test/java/by/onliner/OnlinerTest.java
@@ -7,18 +7,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class OnlinerTest extends BaseTest{
-    public static String aboutCompanyLinkXPath = "//a[@href='https://blog.onliner.by/about']";
-    public static String aboutCompanyPageTitleXPath = "//div[@class='news-header__title']/h1";
 
-    public void openAboutCompanyPage() {
-        WebElement aboutCompanyLink = driver.findElement(By.xpath(aboutCompanyLinkXPath));
-        aboutCompanyLink.click();
-    }
-
-    public String getAboutCompanyPageTitleText() {
-        WebElement aboutCompanyPageTitle = driver.findElement(By.xpath(aboutCompanyPageTitleXPath));
-        return aboutCompanyPageTitle.getText();
-    }
 
     @Test
     @DisplayName("About Company page title IS 'О сайте'")
