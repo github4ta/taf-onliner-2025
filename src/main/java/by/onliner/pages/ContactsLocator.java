649ff3c733b1f5b242c27a56a4fe1457848c1a53
Alena Shushkevich <elenashushkevich7@gmail.com>
2024-02-25 18:22:10 +0300

    added new ContactsLocator class, updated ContactsTest

---
+++ src/main/java/by/onliner/pages/ContactsLocator.java
@@ -0,0 +1,6 @@
+package by.onliner.pages;
+
+public class ContactsLocator {
+    public static final String ContactsHomePageXpath = "\"//*[contains(text(),\\\"Контакты редакции\\\")]\"";
+
+}
--- src/test/java/by/onliner/ContactsTest.java
+++ src/test/java/by/onliner/ContactsTest.java
@@ -1,5 +1,7 @@
 package by.onliner;
 
+import by.onliner.pages.CatalogLocator;
+import by.onliner.pages.ContactsLocator;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
@@ -7,7 +9,7 @@ import org.openqa.selenium.By;
 public class ContactsTest extends BaseTest {
     @Test
     public void testContactsOpened() {
-        driver.findElement(By.xpath("//*[contains(text(),\"Контакты редакции\")]")).click();
+        driver.findElement(ContactsLocator.ContactsHomePageXpath).click();
         Assertions.assertTrue(driver.findElement(By.xpath("//div[@class=\"news-header__title\"]")).getText().contains("Контакты редакции."));
     }
 }
