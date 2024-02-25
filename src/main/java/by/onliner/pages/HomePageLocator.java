a19376d8fffeb1faf96cb76d983da0602b4c3471
viktoryiapazhydayeva <viktoryia_pazhydayeva@epam.com>
2024-02-25 17:25:07 +0300

    Add navigation XPath to HomePageLocator class

--- src/main/java/by/onliner/pages/HomePageLocator.java
+++ src/main/java/by/onliner/pages/HomePageLocator.java
@@ -4,4 +4,7 @@ public class HomePageLocator {
 
     public static final String LOCATOR = "you locator to the link web element";
     public static final String AUTOBARAHOLKA_XPATH = "//ul[@class='b-main-navigation']/li[3]/a";
+
+
+    public static String aboutCompanyLinkXPath = "//a[@href='https://blog.onliner.by/about']";
 }
