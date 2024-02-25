0237b4cd36f4914212cd3eac1494d7d949def8ad
Victoria Vlasova <a5glyk@gmail.com>
2024-02-25 20:06:03 +0300

    refactor classes

--- src/test/java/by/onliner/AutobaraholkaTest.java
+++ src/test/java/by/onliner/AutobaraholkaTest.java
@@ -1,15 +1,18 @@
 package by.onliner;
 
 import by.onliner.pages.AutobaraholkaPage;
+import by.onliner.pages.HomePage;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
 
 public class AutobaraholkaTest extends BaseTest{
 
     @Test
     public void testAutobaraholkaOpened() {
         AutobaraholkaPage autobaraholka = new AutobaraholkaPage(driver);
-        autobaraholka.clickAutobaraholkaBtn();
+        HomePage hp = new HomePage(driver);
+        hp.clickAutobaraholkaBtn();
         autobaraholka.getAutobaraholkaTitle();
         Assertions.assertEquals("Автобарахолка", autobaraholka.getAutobaraholkaTitle());
     }
