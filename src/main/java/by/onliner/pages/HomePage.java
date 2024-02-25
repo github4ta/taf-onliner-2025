bcf492f02f56652778cab63cc184603e745fa1fb
DESKTOP-I2RB543\sweet <olga.abramovich9@gmail.com>
2024-02-25 19:42:12 +0300

    resolve conflict

--- src/main/java/by/onliner/pages/HomePage.java
+++ src/main/java/by/onliner/pages/HomePage.java
@@ -3,9 +3,6 @@ package by.onliner.pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-
 import static by.onliner.pages.HomePageLocator.COPYRIGHTS;
 
 public class HomePage {
@@ -23,15 +20,13 @@ public class HomePage {
     public void vacancyBtnClick() {
         driver.findElement(By.xpath(HomePageLocator.VACANCY_BTN)).click();
     }
+
     public void clickBaraholkaButton(){
         driver.findElement(HomePageLocator.BARAHOLKA_BUTTON_XPATH).click();
     }
-    public void contactsClick(){  driver.findElement(By.xpath(HomePageLocator.CONTACTS_XPATH)).click();}
-    private WebDriver driver;
 
-    public HomePage(WebDriver driver) {
-        this.driver = driver;
-    }
+    public void contactsClick()
+    {  driver.findElement(By.xpath(HomePageLocator.CONTACTS_XPATH)).click();}
 
     public String getCopyrightsText() {
         return driver.findElement(By.cssSelector(COPYRIGHTS)).getText();
