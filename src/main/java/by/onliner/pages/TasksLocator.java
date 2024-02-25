14b53ce078ae35a61694efe4fdae2d56df256dc1
Alyona-Kulachek <kulachek91@inbox.ru>
2024-02-25 23:16:14 +0700

    add TasksLocator class

---
+++ src/main/java/by/onliner/pages/TasksLocator.java
@@ -0,0 +1,6 @@
+package by.onliner.pages;
+
+public class TasksLocator {
+    public static final String SERVICE_SECTION = "(//a[@href='https://s.onliner.by/tasks'])[2]";
+    public static final String HEADER_ORDERS = "//div[@class='service-header__title service-header__title_huge']";
+}
