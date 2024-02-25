c643e42d86e2fa52095db4176e61ba46eb66bda2
Victoria Vlasova <a5glyk@gmail.com>
2024-02-25 16:55:45 +0300

    refactor locators

--- src/main/java/by/onliner/pages/AutobaraholkaLocator.java
+++ src/main/java/by/onliner/pages/AutobaraholkaLocator.java
@@ -1,6 +1,5 @@
 package by.onliner.pages;
 
 public class AutobaraholkaLocator {
-    public static final String AUTOBARAHOLKA_XPATH = "//ul[@class='b-main-navigation']/li[3]/a";
-    public static final String ACTUAL_TITLE_XPATH = "//h1[@class='vehicle-form__title vehicle-form__title_big-alter']";
+    public static final String ACTUAL_TITLE_FOR_AUTOBARAHOLKA_XPATH = "//h1[@class='vehicle-form__title vehicle-form__title_big-alter']";
 }
