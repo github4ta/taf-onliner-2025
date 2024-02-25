c4260f043a2358a100fd3ec25214cd85d07763d4
Victoria Vlasova <a5glyk@gmail.com>
2024-02-25 16:55:16 +0300

    add locator for Autobaraholka

--- src/main/java/by/onliner/pages/HomePageLocator.java
+++ src/main/java/by/onliner/pages/HomePageLocator.java
@@ -3,4 +3,5 @@ package by.onliner.pages;
 public class HomePageLocator {
 
     public static final String LOCATOR = "you locator to the link web element";
+    public static final String AUTOBARAHOLKA_XPATH = "//ul[@class='b-main-navigation']/li[3]/a";
 }
