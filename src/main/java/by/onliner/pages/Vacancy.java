8a43b2d1e6528d29f05ebdbfef156d182b9f43c1
n.khadasevich <nataliabizunkova@gmail.com>
2024-02-25 19:26:40 +0300

    add vacancy test

--- src/main/java/by/onliner/pages/Vacancy.java
+++ src/main/java/by/onliner/pages/Vacancy.java
@@ -1,9 +1,14 @@
 package by.onliner.pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Vacancy {
-    public void vacancyHeaderGetText() {
-        driver.findElement(By.xpath(VacancyLocator.VACANCY_HEADER_XPATH)).getText();
+    ChromeDriver driver;
+    public Vacancy(ChromeDriver driver){
+        this.driver=driver;
+    }
+    public String vacancyHeaderGetText() {
+        return driver.findElement(By.xpath(VacancyLocator.VACANCY_HEADER_XPATH)).getText();
     }
 }
