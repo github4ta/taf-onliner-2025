14f5073f937e6add9908c3dba2815032441f9e18
Alena Shushkevich <elenashushkevich7@gmail.com>
2024-02-25 13:47:51 +0300

    refactor method

--- src/test/java/by/onliner/ContactsTest.java
+++ src/test/java/by/onliner/ContactsTest.java
@@ -3,16 +3,11 @@ package by.onliner;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 
 public class ContactsTest extends BaseTest {
     @Test
     public void testContactsOpened() {
-
-        String redactionContactsXpath = "//*[contains(text(),\"Контакты редакции\")]";
-        String titleContactsPageXpath = "//div[@class=\"news-header__title\"]";
-        driver.findElement(By.xpath(redactionContactsXpath)).click();
-        WebElement titleContactsPage = driver.findElement(By.xpath(titleContactsPageXpath));
-        Assertions.assertTrue(titleContactsPage.getText().contains("Контакты редакции."));
+        driver.findElement(By.xpath("//*[contains(text(),\"Контакты редакции\")]")).click();
+        Assertions.assertTrue(driver.findElement(By.xpath("//div[@class=\"news-header__title\"]")).getText().contains("Контакты редакции."));
     }
 }
