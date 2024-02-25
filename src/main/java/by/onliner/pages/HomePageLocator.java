7cfedce55a27810bc6d3f2805ec09516abfb3402
n.khadasevich <nataliabizunkova@gmail.com>
2024-02-25 17:21:07 +0300

    refactor VacancyTest

--- src/main/java/by/onliner/pages/HomePageLocator.java
+++ src/main/java/by/onliner/pages/HomePageLocator.java
@@ -4,4 +4,5 @@ public class HomePageLocator {
 
     public static final String LOCATOR = "you locator to the link web element";
     public static final String AUTOBARAHOLKA_XPATH = "//ul[@class='b-main-navigation']/li[3]/a";
+    public static final String VACANCY_BTN="//a[@href='https://blog.onliner.by/vacancy']";
 }
