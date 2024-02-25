94430c73f5dbf859bdcae4fa5a68211b453571cc
Alyona-Kulachek <160344995+Alyona-Kulachek@users.noreply.github.com>
2024-02-26 01:38:52 +0700

    UI-0101

--- src/main/java/by/onliner/pages/ReturnPolicyLocator.java
+++ src/main/java/by/onliner/pages/ReturnPolicyLocator.java
@@ -3,6 +3,16 @@ package by.onliner.pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
+import javax.swing.plaf.PanelUI;
+
 public class ReturnPolicyLocator {
     public static final String NEWS_HEADER_TITLE = "//div[@class='news-header__title']";
+    public static final String FIELD_NAME = "//label[@for='id_name']";
+    public static final String FIELD_EMAIL = "//label[@for='id_email']";
+    public static final String FIELD_TYPE_OF_PROBLEM = "//label[@for='id_type']";
+    public static final String FIELD_WHERE = "//label[@for='id_project']";
+    public static final String FIELD_SHORT_DESCRIPTION = "//label[@for='id_subject']";
+    public static final String FIELD_DETAILED_DESCRIPTION = "//label[@for='id_description']";
+    public static final String FIELD_CAPTCHA = "//label[@for='id_captcha']";
+
 }
