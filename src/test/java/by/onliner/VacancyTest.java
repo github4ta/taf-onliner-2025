388291f69aa031764e28d4fe8e211992b40c4a70
n.khadasevich <nataliabizunkova@gmail.com>
2024-02-25 13:18:31 +0300

    add test method testVacancyOpened

--- src/test/java/by/onliner/VacancyTest.java
+++ src/test/java/by/onliner/VacancyTest.java
@@ -1,4 +1,13 @@
 package by.onliner;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
 public class VacancyTest extends BaseTest {
+    @Test
+    public void testVacancyOpened() {
+        driver.findElement(By.xpath("//a[@href='https://blog.onliner.by/vacancy']")).click();
+        Assertions.assertEquals("Вакансии", driver.findElement(By.xpath("//div[@class='news-header__title']/h1")).getText());
+    }
 }
