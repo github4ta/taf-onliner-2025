a1a920c1f9e5bab6648cdb5c00e1bb583a142446
Sergei Tsarik <serge.tsarik@gmail.com>
2024-02-25 16:24:40 +0300

    refactor OnlineTest class

--- src/test/java/by/onliner/OnlinerTest.java
+++ src/test/java/by/onliner/OnlinerTest.java
@@ -3,9 +3,9 @@ package by.onliner;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+class OnlinerTest {
     @Test
     public void testOnliner() {
         WebDriver driver = new ChromeDriver();
