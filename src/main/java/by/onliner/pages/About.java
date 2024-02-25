af4467c435d473aec7858428837048cb67f20a5b
viktoryiapazhydayeva <viktoryia_pazhydayeva@epam.com>
2024-02-25 19:34:39 +0300

    Refactor About and AboutTest classes

--- src/main/java/by/onliner/pages/About.java
+++ src/main/java/by/onliner/pages/About.java
@@ -1,4 +1,19 @@
 package by.onliner.pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class About {
+    WebDriver driver;
+
+    public About(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public String getAboutCompanyPageTitleText() {
+        WebElement aboutCompanyPageTitle = driver.findElement(By.xpath(AboutLocator.aboutCompanyPageTitleXPath));
+        return aboutCompanyPageTitle.getText();
+    }
 }
--- src/test/java/by/onliner/AboutTest.java
+++ src/test/java/by/onliner/AboutTest.java
@@ -1,5 +1,6 @@
 package by.onliner;
 
+import by.onliner.pages.About;
 import by.onliner.pages.AboutLocator;
 import by.onliner.pages.HomePageLocator;
 import org.junit.jupiter.api.*;
@@ -8,19 +9,10 @@ import org.openqa.selenium.WebElement;
 
 public class AboutTest extends BaseTest {
 
-    public void openAboutCompanyPage() {
-        WebElement aboutCompanyLink = driver.findElement(By.xpath(HomePageLocator.aboutCompanyLinkXPath));
-        aboutCompanyLink.click();
-    }
-
-    public String getAboutCompanyPageTitleText() {
-        WebElement aboutCompanyPageTitle = driver.findElement(By.xpath(AboutLocator.aboutCompanyPageTitleXPath));
-        return aboutCompanyPageTitle.getText();
-    }
-
     @Test
     public void testAboutOpened() {
+        About about = new About(driver);
         openAboutCompanyPage();
-        Assertions.assertEquals("О сайте", getAboutCompanyPageTitleText());
+        Assertions.assertEquals("О сайте", about.getAboutCompanyPageTitleText());
     }
 }
