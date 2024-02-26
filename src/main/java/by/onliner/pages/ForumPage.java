2bdfe78a11ac39eb06c73177f0c194d293fd476a
viktoryiapazhydayeva <160345170+viktoryiapazhydayeva@users.noreply.github.com>
2024-02-26 23:09:21 +0300

    RE-0006

--- src/main/java/by/onliner/pages/ForumPage.java
+++ src/main/java/by/onliner/pages/ForumPage.java
@@ -5,26 +5,31 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 public class ForumPage {
-    WebDriver driver;
+    private WebDriver driver;
 
     public ForumPage(WebDriver driver) {
         this.driver = driver;
     }
 
+    private final String TITLE_XPATH = "//h1[@class='m-title']";
+    private final By FIRST_ELEMENT_IN_LIST_IMPORTANT_SECTION_CSS = By.cssSelector("div.h-hottopics>ul>li:nth-child(1)>h3>a");
+    private final By HEADING_FIRST_ELEMENT_IN_LIST_IMPORTANT_SECTION_CSS = org.openqa.selenium.By.cssSelector("h1.m-title>a");
+
+
     public String getText() {
-        WebElement text = driver.findElement(By.xpath(ForumLocator.TITLE_XPATH));
+        WebElement text = driver.findElement(By.xpath(TITLE_XPATH));
         return text.getText();
     }
 
     public String getTextFirstElementInListImportantSection() {
-        return driver.findElement(ForumLocator.FIRST_ELEMENT_IN_LIST_IMPORTANT_SECTION_CSS).getText();
+        return driver.findElement(FIRST_ELEMENT_IN_LIST_IMPORTANT_SECTION_CSS).getText();
     }
 
     public void clickFirstElementInListImportantSection() {
-        driver.findElement(ForumLocator.FIRST_ELEMENT_IN_LIST_IMPORTANT_SECTION_CSS).click();
+        driver.findElement(FIRST_ELEMENT_IN_LIST_IMPORTANT_SECTION_CSS).click();
     }
 
     public String getTextHeadingFirstElementInListImportantSection() {
-        return driver.findElement(ForumLocator.HEADING_FIRST_ELEMENT_IN_LIST_IMPORTANT_SECTION_CSS).getText();
+        return driver.findElement(HEADING_FIRST_ELEMENT_IN_LIST_IMPORTANT_SECTION_CSS).getText();
     }
 }
