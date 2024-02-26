541c30dfc8fc953ed7e624af83733afc8b8d046d
viktoryiapazhydayeva <viktoryia_pazhydayeva@epam.com>
2024-02-26 22:16:57 +0300

    Refactor ForumLocator, ForumPage, ForumTest classes

--- src/main/java/by/onliner/pages/ForumLocator.java
+++ src/main/java/by/onliner/pages/ForumLocator.java
@@ -1,9 +1,5 @@
 package by.onliner.pages;
 
-import org.openqa.selenium.By;
-
 public class ForumLocator {
-    public final static String TITLE_XPATH = "//h1[@class='m-title']";
-    public final static By FIRST_ELEMENT_IN_LIST_IMPORTANT_SECTION_CSS =By.cssSelector("div.h-hottopics>ul>li:nth-child(1)>h3>a");
-    public final static By HEADING_FIRST_ELEMENT_IN_LIST_IMPORTANT_SECTION_CSS =By.cssSelector("h1.m-title>a");
+
 }
--- src/test/java/by/onliner/ForumTest.java
+++ src/test/java/by/onliner/ForumTest.java
@@ -1,18 +1,20 @@
 package by.onliner;
 
-import by.onliner.pages.ForumLocator;
 import by.onliner.pages.ForumPage;
 import by.onliner.pages.HomePage;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
+
 
 public class ForumTest extends BaseTest {
     @Test
     public void testForumOpened() {
-        driver.findElement(By.xpath(HomePageLocator.FORUM_XPATH)).click();
-        driver.findElement(By.xpath(HomePageLocator.PERSONAL_DATA_XPATH)).click();
-        Assertions.assertEquals("Форум", driver.findElement(By.xpath(ForumLocator.TITLE_XPATH)).getText());
+        HomePage homePage = new HomePage(driver);
+        homePage.openForum();
+        ForumPage forumPage = new ForumPage(driver);
+        //driver.findElement(By.xpath(HomePage.FORUM_XPATH)).click();
+        //driver.findElement(By.xpath(HomePageLocator.PERSONAL_DATA_XPATH)).click();
+        Assertions.assertEquals("Форум", forumPage.getText());
     }
 
     @Test
