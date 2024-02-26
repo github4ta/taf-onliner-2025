970df52e3a4de13b0cb943904b335444325d4e1c
Olga Abramovitch <69840953+Olya741@users.noreply.github.com>
2024-02-26 19:17:33 +0300

    UI-0112

--- src/main/java/by/onliner/pages/Tasks.java
+++ src/main/java/by/onliner/pages/Tasks.java
@@ -2,6 +2,10 @@ package by.onliner.pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+
+import static by.onliner.pages.TasksLocator.*;
 
 public class Tasks {
     private WebDriver driver;
@@ -12,5 +16,23 @@ public class Tasks {
 
     public String getTextHeader() {
         return driver.findElement(By.xpath(TasksLocator.HEADER_ORDERS)).getText();
+    }
+
+    public void selectSectionFromList() {
+        WebElement element = driver.findElement(By.xpath(SCROLL_TO_SECTION_TITLE));
+        new Actions(driver).scrollToElement(element).perform();
+        driver.findElement(By.xpath(SUBSECTION_NAME)).click();
+    }
+
+    public void selectCheckbox() {
+        driver.findElement(By.xpath(CHECKBOX)).click();
+    }
+
+    public void selectOffer() {
+        driver.findElement(By.xpath(OFFER_ITEM)).click();
+    }
+
+    public String getServiceDetailText() {
+        return driver.findElement(By.xpath(SERVICE_DETAIL)).getText();
     }
 }
--- src/test/java/by/onliner/TasksTest.java
+++ src/test/java/by/onliner/TasksTest.java
@@ -1,11 +1,9 @@
 package by.onliner;
 
 import by.onliner.pages.HomePage;
-import by.onliner.pages.HomePageLocator;
 import by.onliner.pages.Tasks;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
 
 public class TasksTest extends BaseTest {
 
@@ -15,5 +13,18 @@ public class TasksTest extends BaseTest {
         homePage.clickServiceSection();
         Tasks tasks = new Tasks(driver);
         Assertions.assertEquals("Заказы", tasks.getTextHeader());
+    }
+
+    @Test
+    public void checkTaskDetailsTitle() throws InterruptedException {
+        HomePage homePage = new HomePage(driver);
+        homePage.clickServiceSection();
+        Tasks tasks = new Tasks(driver);
+        tasks.selectSectionFromList();
+        tasks.selectCheckbox();
+        Thread.sleep(2000);
+        tasks.selectOffer();
+        Thread.sleep(3000);
+        Assertions.assertEquals("Подготовка документов для визы", tasks.getServiceDetailText());
     }
 }
