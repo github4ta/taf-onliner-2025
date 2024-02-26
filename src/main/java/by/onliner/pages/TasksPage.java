3e0762a0380af939a06f402d792b1b24c70ec414
Victoria Vlasova <a5glyk@gmail.com>
2024-02-26 23:43:05 +0300

    refactor TaskTest class

--- src/main/java/by/onliner/pages/TasksPage.java
+++ src/main/java/by/onliner/pages/TasksPage.java
@@ -6,18 +6,18 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 
 public class TasksPage {
-    public static final String SUBSECTION_NAME = "//a[text()='??????']/ancestor::div[@class='service-form__collapse-header']";
-    public static final String HEADER_ORDERS = "//div[@class='service-header__title service-header__title_huge']";
-    public static final String MINSK_FILTER = "//li[@data-ng-repeat='itemId in searchFilterRegions.priorItemsIds']/label/span/span[@class='i-checkbox__faux'][1]";
-    public static final String SERVICES_FOR_ANIMALS = "//*[@id=\"service-list\"]/div[2]/div/div/div[2]/div[2]/div[4]/div/div[2]/div/div/div[15]/div[1]/a";
-    public static final String HAIRCUT_FOR_HOME_ANIMALS = "//*[@id=\"service-list\"]/div[2]/div/div/div[2]/div[2]/div[4]/div/div[2]/div/div/div[15]/div[2]/ul/li[1]/label/span/span[1]";
-    public static final String ACTIVE_STATUS = "//*[@id=\"service-list\"]/div[2]/div/div/div[2]/div[2]/div[5]/div/div[2]/ul/li[1]/label/span/span[1]";
-    public static final String FIRST_ITEM_IN_SEARCH = "//*[@id=\"service-list\"]/div[2]/div/div/div[3]/search-tasks-list/div/div/div[1]/search-tasks-item/div/div/div[1]/div/a/span[2]/span[1]";
-    public static final String TITLE = "//*[@id=\"container\"]/div/div[2]/div/div/div/div/div/div[1]/div/div/div/div[1]/div";
-    public static final String SCROLL_TO_SECTION_TITLE = "//div[@class='service-form__label-title' and text()='??????']";
-    public static final String CHECKBOX = "//div[contains(@class,'service-form__collapse-item_opened')]//li[2]";
-    public static final String OFFER_ITEM = "//div[@class='service-offers__data']/a";
-    public static final String SERVICE_DETAIL = "//div[@class='service-summary__details-list']/a";
+    private final String SUBSECTION_NAME = "//a[text()='??????']/ancestor::div[@class='service-form__collapse-header']";
+    private final String HEADER_ORDERS = "//div[@class='service-header__title service-header__title_huge']";
+    private final String MINSK_FILTER = "//li[@data-ng-repeat='itemId in searchFilterRegions.priorItemsIds']/label/span/span[@class='i-checkbox__faux'][1]";
+    private final String SERVICES_FOR_ANIMALS = "//*[@id=\"service-list\"]/div[2]/div/div/div[2]/div[2]/div[4]/div/div[2]/div/div/div[15]/div[1]/a";
+    private final String HAIRCUT_FOR_HOME_ANIMALS = "//*[@id=\"service-list\"]/div[2]/div/div/div[2]/div[2]/div[4]/div/div[2]/div/div/div[15]/div[2]/ul/li[1]/label/span/span[1]";
+    private final String ACTIVE_STATUS = "//*[@id=\"service-list\"]/div[2]/div/div/div[2]/div[2]/div[5]/div/div[2]/ul/li[1]/label/span/span[1]";
+    private final String FIRST_ITEM_IN_SEARCH = "//*[@id=\"service-list\"]/div[2]/div/div/div[3]/search-tasks-list/div/div/div[1]/search-tasks-item/div/div/div[1]/div/a/span[2]/span[1]";
+    private final String TITLE = "//*[@id=\"container\"]/div/div[2]/div/div/div/div/div/div[1]/div/div/div/div[1]/div";
+    private final String SCROLL_TO_SECTION_TITLE = "//div[@class='service-form__label-title' and text()='??????']";
+    private final String CHECKBOX = "//div[contains(@class,'service-form__collapse-item_opened')]//li[2]";
+    private final String OFFER_ITEM = "//div[@class='service-offers__data']/a";
+    private final String SERVICE_DETAIL = "//div[@class='service-summary__details-list']/a";
     private WebDriver driver;
 
     public TasksPage(WebDriver driver) {
@@ -38,12 +38,12 @@ public class TasksPage {
         Thread.sleep(20000);
     }
 
-    public void clickHaircutForAnimals() throws InterruptedException{
+    public void clickHaircutForAnimals() throws InterruptedException {
         driver.findElement(By.xpath(HAIRCUT_FOR_HOME_ANIMALS)).click();
         Thread.sleep(20000);
     }
 
-    public void clickActiveStatus() throws InterruptedException{
+    public void clickActiveStatus() throws InterruptedException {
         driver.findElement(By.xpath(ACTIVE_STATUS)).click();
         Thread.sleep(20000);
     }
