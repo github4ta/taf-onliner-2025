97fc10690e311c01d50462c52eeae7a6ef4c2c4a
daryaPadaliak <160344751+daryaPadaliak@users.noreply.github.com>
2024-02-27 10:03:33 +0100

    RE-0007

--- src/main/java/by/onliner/pages/CatalogPage.java
+++ src/main/java/by/onliner/pages/CatalogPage.java
@@ -8,6 +8,7 @@ public class CatalogPage {
     private final String GARDEN_AND_HOME = "//li[@data-id=\"5\"]";
     private final String DECOR_AND_INTERIOR = "//*[@id=\"container\"]/div/div/div/div/div[1]/div[4]/div/div[7]/div[1]/div/div[10]/div[1]";
     private final String HOME_FLOWERS = "//*[@id=\"container\"]/div/div/div/div/div[1]/div[4]/div/div[7]/div[1]/div/div[10]/div[2]/div/a[6]/span/span[2]";
+    private final String PRIVACY_POLICY_ACCEPT = "/html/body/div[6]/div[2]/div[1]/div[2]/div[2]/button[1]/p";
     private WebDriver driver;
 
     public CatalogPage(WebDriver driver) {
@@ -29,5 +30,8 @@ public class CatalogPage {
 
     public void clickHouseFlowersItem() {
         driver.findElement(By.xpath(HOME_FLOWERS)).click();
+    }
+    public void acceptPrivacyPolicy() {
+        driver.findElement(By.xpath(PRIVACY_POLICY_ACCEPT)).click();
     }
 }
--- src/main/java/by/onliner/pages/HouseplantsLocators.java
+++
@@ -1,8 +0,0 @@
-package by.onliner.pages;
-
-public class HouseplantsLocators {
-
-    public static final String LOCATION_POP_UP = "//span[@class='button-style button-style_primary button-style_small-alter catalog-form__button']";
-    public static final String HOUSEPLANTS_HEADER = "//h1[@class=\"catalog-form__title catalog-form__title_big-alter\"]";
-
-}
