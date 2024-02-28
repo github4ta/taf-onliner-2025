b93d1ff6e3e10d59ee8eca9d265e24fdf80254ff
viktoryiapazhydayeva <viktoryia_pazhydayeva@epam.com>
2024-02-28 20:30:21 +0300

    Add draft getRandomSurname method

--- src/main/java/by/onliner/domain/Users.java
+++ src/main/java/by/onliner/domain/Users.java
@@ -1,7 +1,13 @@
 package by.onliner.domain;
 
 import by.onliner.driver.Driver;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class Users {
     private WebDriver driver;
@@ -10,6 +16,15 @@ public class Users {
         this.driver = Driver.getDriver();
     }
 
-    private final String SURNAME_LIST_XPATH = "//ul/a";
     private final String SURNAME_URL = "https://eslyes.com/namesdict/100_last_names.htm";
+    private final String SURNAME_LIST_XPATH = "//ul/a";
+
+
+    public String getRandomSurname() {
+        driver.get(SURNAME_URL);
+        List<WebElement> surnameList = driver.findElements(By.xpath(SURNAME_LIST_XPATH));
+        Random random = new Random();
+        WebElement randomSurnameIndex = surnameList.get(random.nextInt(surnameList.size()));
+        return randomSurnameIndex.getText();
+    }
 }
