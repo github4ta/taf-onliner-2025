003779208e7c0fabc708f9444a26c653d7ee6398
usermaksimnovikov <160344866+usermaksimnovikov@users.noreply.github.com>
2024-02-28 13:13:17 +0300

    RE-0021

--- src/test/java/by/onliner/AboutTest.java
+++ src/test/java/by/onliner/AboutTest.java
@@ -1,6 +1,6 @@
 package by.onliner;
 
-import by.onliner.pages.About;
+import by.onliner.pages.AboutPage;
 import by.onliner.pages.HomePage;
 import org.junit.jupiter.api.*;
 
@@ -8,9 +8,9 @@ public class AboutTest extends BaseTest {
 
     @Test
     public void testAboutOpened() {
-        About about = new About(driver);
         HomePage homePage = new HomePage(driver);
+        AboutPage aboutPage = new AboutPage(driver);
         homePage.openAboutCompanyPage();
-        Assertions.assertEquals("О сайте", about.getAboutCompanyPageTitleText());
+        Assertions.assertEquals("О сайте", aboutPage.getTextCompanyPageTittle());
     }
 }
