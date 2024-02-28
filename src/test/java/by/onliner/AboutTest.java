5435f2d6ceec65b7575eec49739d689657bcac3a
usermaksimnovikov <160344866+usermaksimnovikov@users.noreply.github.com>
2024-02-28 14:23:13 +0300

    RE-0033

--- src/test/java/by/onliner/AboutTest.java
+++ src/test/java/by/onliner/AboutTest.java
@@ -8,8 +8,8 @@ public class AboutTest extends BaseTest {
 
     @Test
     public void testAboutOpened() {
-        HomePage homePage = new HomePage(driver);
-        AboutPage aboutPage = new AboutPage(driver);
+        HomePage homePage = new HomePage();
+        AboutPage aboutPage = new AboutPage();
         homePage.openAboutCompanyPage();
         Assertions.assertEquals("О сайте", aboutPage.getTextCompanyPageTittle());
     }
