9d64b8239db11d4a1b82273f15247b1415d49359
viktoryiapazhydayeva <viktoryia_pazhydayeva@epam.com>
2024-02-28 20:06:23 +0300

    Add XPathes to Users class

--- src/main/java/by/onliner/domain/Users.java
+++ src/main/java/by/onliner/domain/Users.java
@@ -1,4 +1,15 @@
 package by.onliner.domain;
 
+import by.onliner.driver.Driver;
+import org.openqa.selenium.WebDriver;
+
 public class Users {
+    private WebDriver driver;
+
+    public Users() {
+        this.driver = Driver.getDriver();
+    }
+
+    private final String SURNAME_LIST_XPATH = "//ul/a";
+    private final String SURNAME_URL = "https://eslyes.com/namesdict/100_last_names.htm";
 }
