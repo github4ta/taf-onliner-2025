3ab3eff9f44c90af9c9202056e39a6d082a4a09d
viktoryiapazhydayeva <160345170+viktoryiapazhydayeva@users.noreply.github.com>
2024-03-05 12:23:27 +0300

    JJ-0002

--- src/test/java/it/justjoin/api/LoginTest.java
+++ src/test/java/it/justjoin/api/LoginTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 public class LoginTest {
     @Test
@@ -18,5 +19,25 @@ public class LoginTest {
                     post("https://profile.justjoin.it/api/justjoinit/authentication/login").
                 then().log().all().
                     statusCode(401);
+    }
+
+    @Test
+    @DisplayName("Login with empty Email field")
+
+    public void testLogin2() {
+
+        String bodyLogin2 = "{\n" +
+                "    \"email\": \"\",\n" +
+                "    \"password\": \"1w1ws1we1e\"\n" +
+                "}";
+        given().
+                body(bodyLogin2).
+                contentType("application/json").
+        when().
+                post("https://profile.justjoin.it/api/justjoinit/authentication/login").
+        then().
+                statusCode(422).
+                body("[0].property", equalTo("Email")).
+                body("[0].error", equalTo("'Email' must not be empty."));
     }
 }
