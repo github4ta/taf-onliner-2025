17c0ad82d0973bfd550daba0f82941f65f675df8
viktoryiapazhydayeva <viktoryia_pazhydayeva@epam.com>
2024-03-05 12:16:11 +0300

    Add testLogin2 method

--- src/test/java/it/justjoin/api/LoginTest.java
+++ src/test/java/it/justjoin/api/LoginTest.java
@@ -1,4 +1,29 @@
 package it.justjoin.api;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
 public class LoginTest {
+    @Test
+    @DisplayName("Login with empty Email field")
+
+    public void testLogin2() {
+
+        String bodyLogin2 = "{\n" +
+                "    \"email\": \"\",\n" +
+                "    \"password\": \"1w1ws1we1e\"\n" +
+                "}";
+        given().
+                body(bodyLogin2).
+                contentType("application/json").
+        when().
+                post("https://profile.justjoin.it/api/justjoinit/authentication/login").
+        then().
+                statusCode(422).
+                body("[0].property", equalTo("Email")).
+                body("[0].error", equalTo("'Email' must not be empty."));
+    }
 }
