4b5669b4ebdb5d9443b1560ad44766c340f06593
DESKTOP-I2RB543\sweet <olga.abramovich9@gmail.com>
2024-03-06 23:54:57 +0300

    Add testPageHeaderAndUrl()

--- src/test/java/it/justjoin/ui/LoginTest.java
+++ src/test/java/it/justjoin/ui/LoginTest.java
@@ -1,4 +1,27 @@
 package it.justjoin.ui;
 
-public class LoginTest {
+import it.justjoin.pages.HomePage;
+import it.justjoin.pages.ProfilePage;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class LoginTest extends BaseTest {
+
+    @Test
+    @DisplayName("Check page header and url")
+    public void testPageHeaderAndUrl() {
+        HomePage homePage = new HomePage();
+        homePage.clickSignInBtn();
+        homePage.clickSignInCandidateBtn();
+        ProfilePage profilePage = new ProfilePage();
+
+        assertAll(
+                () -> assertEquals("Sign in or sign up", profilePage.getTextSignInToAccount()),
+                () -> assertEquals("https://profile.justjoin.it/login", profilePage.getCurrentUrl())
+        );
+    }
 }
