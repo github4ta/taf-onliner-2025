639f8667b64a3cfb15dceaeed2b2c33a632ec47a
viktoryiapazhydayeva <viktoryia_pazhydayeva@epam.com>
2024-03-06 22:15:48 +0300

    Add testLoginWithEmptyPassword method

--- src/test/java/it/justjoin/ui/LoginTest.java
+++ src/test/java/it/justjoin/ui/LoginTest.java
@@ -1,4 +1,24 @@
 package it.justjoin.ui;
 
+import by.onliner.ui.BaseTest;
+import it.justjoin.pages.HomePage;
+import it.justjoin.pages.ProfilePage;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class LoginTest {
+
+    @Test
+    @DisplayName("Candidate login with empty Password field")
+    public void testLoginWithEmptyPassword(){
+        HomePage homePage = new HomePage();
+        homePage.clickSignInBtn();
+        homePage.clickSignInCandidateBtn();
+        ProfilePage profilePage = new ProfilePage();
+        profilePage.clickSignInUsingAddressEmailBtn();
+        profilePage.sendKeysEmailField("Test1020@user.com");
+        profilePage.clickSingInBtn();
+        Assertions.assertEquals("This field is required.", profilePage.getPasswordValidationMsgText());
+    }
 }
