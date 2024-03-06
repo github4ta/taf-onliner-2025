8d4aef41c602c1eb47b47fb01bd3cf9db8e5426c
Maksim Novikov <moksemnovikov@gmail.com>
2024-03-06 23:23:44 +0300

    JJ-0109

--- src/test/java/it/justjoin/ui/LoginTest.java
+++ src/test/java/it/justjoin/ui/LoginTest.java
@@ -2,6 +2,7 @@ package it.justjoin.ui;
 
 import by.onliner.ui.BaseTest;
 import it.justjoin.pages.HomePage;
+import it.justjoin.pages.PanelPage;
 import it.justjoin.pages.ProfilePage;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -20,5 +21,16 @@ public class LoginTest extends BaseTest {
         profilePage.sendKeysEmailField("Test1020@user.com");
         profilePage.clickSingInBtn();
         Assertions.assertEquals("This field is required.", profilePage.getPasswordValidationMsgText());
+    }
+    @Test
+    @DisplayName("Sign in Employer panel with empty Email and Password fields")
+    public void testEmployerLoginWithEmptyEmailAndPassword() {
+        HomePage homePage = new HomePage();
+        homePage.clickSignInBtn();
+        homePage.clickSignInEmployerBtn();
+        PanelPage panelPage = new PanelPage();
+        panelPage.clickSignIn();
+        Assertions.assertEquals("This field is required\n" +
+                "This field is required", panelPage.getTextEmailErrorMessage() + "\n" + panelPage.getTextPasswordErrorMessage());
     }
 }
