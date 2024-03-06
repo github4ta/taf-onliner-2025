f42831c18237e17aba038b161c2b28a99f8b21cb
usermaksimnovikov <160344866+usermaksimnovikov@users.noreply.github.com>
2024-03-06 23:24:36 +0300

    JJ-0109

--- src/main/java/it/justjoin/pages/HomePage.java
+++ src/main/java/it/justjoin/pages/HomePage.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class HomePage {
-    WebDriver driver;
+    private WebDriver driver;
 
     public HomePage() {
         this.driver = Driver.getDriver();
