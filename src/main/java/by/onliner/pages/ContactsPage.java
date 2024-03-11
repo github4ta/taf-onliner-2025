4c31354ee6ee35312918b3998aabba2ab0179c53
DESKTOP-I2RB543\sweet <olga.abramovich9@gmail.com>
2024-03-11 22:25:24 +0300

    Refactor

--- src/main/java/by/onliner/pages/ContactsPage.java
+++ src/main/java/by/onliner/pages/ContactsPage.java
@@ -5,16 +5,12 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class ContactsPage {
-    private final String CONTACTS_HOME_PAGE_XPATH = "/div[@class=\"news-header__title\"]";
-    private final String CONTACTS_OF_EDITORIAL_OFFICE = "//div[@class='footer-style__part footer-style__part_1']/ul/li[2]/a";
+    private final String CONTACTS_HOME_PAGE_XPATH = "//div[@class='news-header__title']";
     private final String EMAIL_OF_ONLINER = "//div[@class='news-text']/p[7]/strong/a";
     private WebDriver driver;
+
     public ContactsPage() {
         this.driver = Driver.getDriver();
-    }
-
-    public ContactsPage(WebDriver driver) {
-        this.driver = driver;
     }
 
     public String getEmailOfOnliner() {
@@ -22,17 +18,6 @@ public class ContactsPage {
     }
 
     public String getCONTACTS_HOME_PAGE_XPATH() {
-        return CONTACTS_HOME_PAGE_XPATH;
-    }
-
-    public String getCONTACTS_OF_EDITORIAL_OFFICE() {
-        return CONTACTS_OF_EDITORIAL_OFFICE;
-    }
-
-    public String getEMAIL_OF_ONLINER() {
-        return EMAIL_OF_ONLINER;
-    }
-    public void clickContactsOfEditorialOffice () {
-        driver.findElement(By.xpath(CONTACTS_OF_EDITORIAL_OFFICE)).click();;
+        return driver.findElement(By.xpath(CONTACTS_HOME_PAGE_XPATH)).getText();
     }
 }
