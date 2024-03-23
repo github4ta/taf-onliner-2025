37587f1cbfdc40c2fa0fcd3f2e21a5fbb084ee00
daryaPadaliak <darya.padaliak@gmail.com>
2024-03-23 18:52:59 +0100

    refactor testLogin6

--- src/main/java/it/justjoin/api/LoginService.java
+++ src/main/java/it/justjoin/api/LoginService.java
@@ -27,4 +27,10 @@ public class LoginService {
                 "}";
         return body;
     }
+    public static String getBody(String email, String password){
+        return String.format("{\n" +
+                "\"email\": \"%s\",\n" +
+                "\"password\": \"%s\"\n" +
+                "}", email, password);
+    }
 }
