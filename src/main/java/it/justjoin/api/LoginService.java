50b76a8113ba8adb288f22f544f85e0669a338a0
Yuliya Abramovitch <yuabramovitch@gmail.com>
2024-03-23 20:12:03 +0300

    Refactor testLogin1

--- src/main/java/it/justjoin/api/LoginService.java
+++ src/main/java/it/justjoin/api/LoginService.java
@@ -1,4 +1,8 @@
 package it.justjoin.api;
 
 public class LoginService {
+    public static final String BODY_1 = "{\n" +
+            " \"email\": \"test@test.com\",\n" +
+            " \"password\": \"1w1ws1we1e\"\n" +
+            "}";
 }
