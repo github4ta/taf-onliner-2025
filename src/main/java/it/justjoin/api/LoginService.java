86cbb3c92d24dc48e9dbbdfffdc81cec580e597e
Natalia Gvozdeva <gvozdeva-natalia@mail.ru>
2024-03-23 20:22:03 +0300

    refactor/body2

--- src/main/java/it/justjoin/api/LoginService.java
+++ src/main/java/it/justjoin/api/LoginService.java
@@ -5,4 +5,9 @@ public class LoginService {
             " \"email\": \"test@test.com\",\n" +
             " \"password\": \"1w1ws1we1e\"\n" +
             "}";
+
+    public static final String BODY_2 = "{\n" +
+            "    \"email\": \"\",\n" +
+            "    \"password\": \"1w1ws1we1e\"\n" +
+            "}";
 }
